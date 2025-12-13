b2d0657af4458b03c6b569e8ee343eb5e0152165
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-14 01:43:18 +0200

    update header and footer components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBagShopping,
+  faCartShopping,
+  faRightToBracket,
+  faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex items-center justify-between px-[10%] pt-5 max-[600px]:justify-center max-[600px]:px-0 bg-teal-500">
+      <Link href="/" className="flex items-center gap-1 max-[600px]:hidden">
+        <FontAwesomeIcon icon={faBagShopping} className="text-2xl mr-1" />
+        <div>
+          <span className="font-bold">AWU</span>
+          <p className="tracking-wide text-sm">Shopping</p>
+        </div>
+      </Link>
+
+      <nav className="flex items-center gap-4 max-[600px]:flex-wrap max-[600px]:justify-center">
+        <Link
+          href="/cart"
+          className="relative flex items-center gap-1 bg-(--bodyFont) text-(--blue) text-sm px-4 py-2 rounded-full"
+        >
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="text-(--blue) w-3"
+          />
+          $0.00
+          <span className="absolute -top-1 right-0 w-5 h-5 text-[0.7rem] font-bold bg-pink-500 text-white flex items-center justify-center rounded-full">
+            2
+          </span>
+        </Link>
+
+        <Link
+          href="/signin"
+          className="flex items-center gap-1 border border-(--bodyFont) px-4 py-2 text-xs font-medium rounded-full transition hover:bg-(--bodyFont) hover:text-(--blue)"
+        >
+          <FontAwesomeIcon icon={faRightToBracket} className="w-3" />
+          Sign in
+        </Link>
+
+        <Link
+          href="/register"
+          className="flex items-center gap-1 border border-(--bodyFont) px-4 py-2 text-xs font-medium rounded-full transition hover:bg-(--bodyFont) hover:text-(--blue)"
+        >
+          <FontAwesomeIcon icon={faUserPlus} className="w-3" />
+          Register
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
