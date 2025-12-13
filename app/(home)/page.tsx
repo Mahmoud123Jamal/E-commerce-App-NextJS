e8047d2eba0d48e02802f1699f59142240fd2aff
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-14 01:46:51 +0200

    move in (home)

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+export default async function Post() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
