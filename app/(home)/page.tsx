7dc4bf8a7560efb9f80d5fc1accd2f33b3c28e5b
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-14 03:48:29 +0200

    remove suspence and use dynamic

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,14 @@
-import { Suspense } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import Products from "./Products";
-import Loading from "./Loading";
+import Loading from "./loading";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import dynamic from "next/dynamic";
+
+const Products = dynamic(() => import("./Products"), {
+  ssr: true,
+  loading: () => <Loading />,
+});
 
 export default function Home() {
   return (
@@ -14,11 +18,11 @@ export default function Home() {
         style={{ backgroundImage: "url(/images/bac.jpg)" }}
       >
         <Header />
-        <section className="absolute top-1/2 left-[10%] -translate-y-1/2 flex flex-col text-white text-2xl space-y-2 sm:text-xl">
+        <section className="absolute top-1/2 left-[10%] -translate-y-1/2 flex flex-col text-black text-2xl space-y-2 sm:text-xl">
           <p className="uppercase text-2xl sm:text-xl">Lifestyle collection</p>
           <p className="mt-2 font-bold text-3xl sm:text-2xl">MEN</p>
           <p className="mt-1 font-bold text-2xl sm:text-xl">
-            SALE UP TO <span className="text-crimson ml-2">30% OFF</span>
+            SALE UP TO <span className="text-red-600 ml-2">30% OFF</span>
           </p>
           <p className="text-xl sm:text-lg mt-1">
             Get Free Shipping on orders over $99.00
@@ -34,9 +38,7 @@ export default function Home() {
           <FontAwesomeIcon icon={faCheck} className="w-6 mr-4 text-green-500" />
           Recommended for you
         </h1>
-        <Suspense fallback={<Loading />}>
-          <Products />
-        </Suspense>
+        <Products />
       </main>
 
       <Footer />
